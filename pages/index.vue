476c17f8a169f1fb9b1b50800119f71ac076cf49
yokoyama takuji <gato.02010417@gmail.com>
2021-01-18 06:32:20 +0900

    Step33 task7 トップページで猫のデータの配列を取得するように修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,98 +1,39 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <div :class="$style.cats" v-for="cat in cats" :key="cat">
+      <div>ねこ：{{ cat.url }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo,
+  data() {
+    return {
+      cats: [],
+    }
   },
-
-  mounted() {
-    const foo = 'foo'
-    console.log(foo)
+  async created() {
+    try {
+      const response = await this.$axios.get(
+        'https://jsondata.okiba.me/v1/json/7lV2J201227155106'
+      )
+      // console.log(response)
+      this.cats = response.data.results
+    } catch (err) {
+      const res = err.response
+      // console.log(res)
+      alert(res)
+    }
   },
 }
 </script>
+
+<style lang="scss" module>
+.cats {
+  margin: 10px 0;
+  padding: 8px;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+}
+</style>
