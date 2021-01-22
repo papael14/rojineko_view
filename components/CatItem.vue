6a3f3e165c0f62cb14842f07da6ee40763846ef1
yokoyama takuji <gato.02010417@gmail.com>
2021-01-23 06:00:53 +0900

    レスポンシブデザイン化

--- components/CatItem.vue
+++ components/CatItem.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <div :class="$style.cats" v-for="cat in cats" :key="cat.uuid">
-      <div>
-        <img :src="cat.url" :alt="cat.uuid" :class="$style.cats_image" />
-      </div>
-      <div>撮影場所：{{ cat.address }}</div>
-      <div>撮影日：{{ cat.createDate | moment }}</div>
-    </div>
+    <v-row>
+      <v-col
+        cols="12"
+        sm="12"
+        md="12"
+        lg="6"
+        xl="6"
+        :class="$style.cats"
+        v-for="cat in cats"
+        :key="cat.uuid"
+      >
+        <div>
+          <img :src="cat.url" :alt="cat.uuid" :class="$style.cats_image" />
+          <div>撮影場所：{{ cat.address }}</div>
+          <div>撮影日：{{ cat.createDate | moment }}</div>
+        </div>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
